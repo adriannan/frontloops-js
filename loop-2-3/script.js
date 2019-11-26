4ef23d3da3d1082802ac2169a3ef83c32ed161c1
Adrianna Rochmalska <ro.adriannaa@gmail.com>
2019-11-26 15:21:34 +0100

    another way / transform

--- loop-2-3/script.js
+++ loop-2-3/script.js
@@ -2,32 +2,37 @@ const btnPrev = document.querySelector(".btn-prev");
 const btnNext = document.querySelector(".btn-next");
 const cards = [...document.querySelectorAll(".card")];
 const slider = document.querySelector(".slider");
+const sliderContent = document.querySelector(".content");
 
-let scroll = -1;
+let index = 0;
+let offsetLeft = cards[0].offsetLeft;
+let scroll = 0;
+let maxScroll = 0;
 
-function setPos() {
-  if (scroll < 0) return (slider.scrollLeft = 0);
-  slider.scrollLeft = cards[scroll].offsetWidth + cards[scroll].offsetLeft;
-}
-
-slider.addEventListener("scroll", setPos);
+cards.forEach(card => {
+  maxScroll = maxScroll + card.offsetWidth + offsetLeft;
+});
 
 btnNext.addEventListener("click", () => {
-  if (scroll === -1) {
-    btnPrev.classList.toggle("btn-disable");
-  } else if (scroll === 3) btnNext.classList.toggle("btn-disable");
-  scroll++;
-  setPos();
-});
-btnPrev.addEventListener("click", () => {
-  scroll--;
-  if (scroll === -1) {
-    slider.scrollLeft = 0;
-    btnPrev.classList.toggle("btn-disable");
-  } else if (scroll === 3) {
-    btnNext.classList.toggle("btn-disable");
-    setPos();
-  } else {
-    setPos();
+  btnPrev.style.display = "block";
+
+  if (scroll > maxScroll - 0.72 * slider.offsetWidth) {
+    btnNext.style.display = "none";
+    sliderContent.style.transform = `translateX(-${scroll}px)`;
   }
+
+  scroll = scroll + cards[index].offsetWidth + offsetLeft;
+  sliderContent.style.transform = `translateX(-${scroll}px)`;
+  index++;
+});
+
+btnPrev.addEventListener("click", () => {
+  btnNext.style.display = "block";
+  if (scroll < cards[1].offsetWidth) {
+    btnPrev.style.display = "none";
+  }
+  index--;
+
+  scroll = scroll - cards[index].offsetWidth - offsetLeft;
+  sliderContent.style.transform = `translateX(-${scroll}px)`;
 });
